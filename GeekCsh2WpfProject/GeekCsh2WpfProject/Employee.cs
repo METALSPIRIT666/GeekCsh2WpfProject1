ad356498f0a06b5a048359e5f34b648883210455
METALSPIRIT666 <anabatov@mail.ru>
2018-06-30 14:52:04 +0300

    Добавлен базовый функционал: департаменты и сотрудники, а также способ их мониторинга.

---
+++ GeekCsh2WpfProject/GeekCsh2WpfProject/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekCsh2WpfProject
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+        public Department Department; 
+
+        public override string ToString() => $"{Id}\t{Name}\t{Age}\t{Salary}";
+    }
+}
--- GeekCsh2WpfProject/GeekCsh2WpfProject/MainWindow.xaml.cs
+++ GeekCsh2WpfProject/GeekCsh2WpfProject/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,36 @@ namespace GeekCsh2WpfProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Department> departments = new ObservableCollection<Department>()
+        {
+            new Department() {Id = 1, Name = "Programmers", Members = 
+                new ObservableCollection<Employee>()
+                {
+                    new Employee() {Id = 1, Name = "Empl1", Age = 25, Salary = 80000},
+                    new Employee() {Id = 3, Name = "Empl3", Age = 35, Salary = 100000},
+                    new Employee() {Id = 4, Name = "Empl4", Age = 21, Salary = 60000}
+                }},
+            new Department() {Id = 2, Name = "Workers", Members =
+                new ObservableCollection<Employee>()
+                {
+                    new Employee() {Id = 2, Name = "Empl2", Age = 46, Salary = 25000},
+                    new Employee() {Id = 5, Name = "Empl5", Age = 28, Salary = 20000}
+                }},
+        };
+        
         public MainWindow()
         {
             InitializeComponent();
+            cbDepartment.ItemsSource = departments;
+            cbDepartment.SelectionChanged += cbDepartmentSelectionChanged;
+            cbDepartment.SelectedIndex = 0;
+        }
+
+        private void cbDepartmentSelectionChanged(object sender,
+            SelectionChangedEventArgs e)
+        {
+            ComboBox cmb = (ComboBox)sender;
+            lbEmployee.ItemsSource = departments.ElementAt(cmb.SelectedIndex).Members;
         }
     }
 }
